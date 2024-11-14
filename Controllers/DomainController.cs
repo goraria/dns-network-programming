2aea71501c1fa6b1d60fa146d74fbba5a3bed07e
DinhTQ <caotran293@gmail.com>
2024-11-14 17:21:43 +0700

    Zamcha - Add controller

---
+++ Controllers/DomainController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dns_network_programming.Controllers
+{
+  public class DomainController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
